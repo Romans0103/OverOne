937788bad2a4a770a78bdf426cba347e63b30d34
Romans0103 <102057532+Romans0103@users.noreply.github.com>
2022-10-06 19:39:16 +0300

    Update hw_sqlite_2.py

--- hw_sqlite_2.py
+++ hw_sqlite_2.py
@@ -1,19 +1,43 @@
 import sqlite3
 
-conn = sqlite3.connect('База Данных.db')
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS WORDS(word TEXT) ''')
-cursor.execute('''CREATE TABLE IF NOT EXISTS DIGIT(digit INTEGER) ''')
+with sqlite3.connect('database.db') as db:
+    cursor = db.cursor()
+    query = '''
+    CREATE TABLE IF NOT EXISTS WORDS(
+        id INTEGER PRIMARY KEY,
+        words TEXT
+    );
+    CREATE TABLE IF NOT EXISTS DIGITS(
+        id INTEGER PRIMARY KEY,
+        digits INTEGER
+    )   
+    '''
+    cursor.executescript(query)
+
 
 spisok = ['white', 'black', 'red', 'yellow', 'orange', 'green', 'blue', 38, 75, 15, 24, 2, 99]
-for element in spisok:
-    if isinstance(element, str):
-        cursor.execute(f'INSERT INTO WORDS  VALUES ("{element}"),')
-        cursor.execute(f'INSERT INTO DIGITS VALUES ("{len(element)},")')
-    elif element % 2:
-        cursor.execute(f'INSERT INTO WORDS VALUES ("нечётное")')
+for i in spisok:
+    if type(i) == str:
+        cursor.execute('''INSERT INTO WORDS(words)  VALUES(?)''', (i,))
+        db.commit()
+        cursor.execute('''INSERT INTO DIGITS(digits) VALUES(?)''', (len(i),))
+        db.commit()
+    elif type(i) == int:
+        if i % 2 == 0:
+            cursor.execute('''INSERT INTO DIGITS(digits) VALUES(?)''', (i,))
+            db.commit()
+        else:
+            cursor.execute('''INSERT INTO DIGITS(digits) VALUES('нечётное')''')
+            db.commit()
     else:
-        cursor.execute(f'INSERT INTO DIGITS VALUES ("{element}",)')
-    conn.commit()
-k = cursor.fetchall()
-print(k)
+        continue
+cursor.execute('SELECT COUNT (*) FROM DIGITS')
+count = cursor.fetchone()
+for i in count:
+    if i > 5:
+        cursor.execute('DELETE FROM WORDS WHERE id = 1')
+        db.commit()
+    else:
+        cursor.execute('UPDATE WORDS SET words = "hello" WHERE id = 1')
+        db.commit()
+    print(f'Строк во второй таблице: {count[0]}.')
